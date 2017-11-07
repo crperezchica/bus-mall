8f09bfb41fcaa781f0ff54158e8b7548c2d401bd
Carmen Perezchica <crperezchica@gmail.com>
2017-11-07 05:56:41 -0800

    created a file for my worker functions

---
+++ product.js
@@ -0,0 +1,25 @@
+
+function Product(type, src) {
+    this.type = type;
+    this.src = './images/' + src;
+    this.vote = 0;
+    this.display = 0;
+
+    // allProducts.push(this);
+}
+
+Product.prototype.wasVote = function (){
+    this.vote += 1;
+};
+
+Product.prototype.wasDisplay = function (){
+    this.display += 1;
+};
+
+Product.prototype.render = function (){
+    const ele = document.createElement('img');
+    ele.src = this.src;
+    ele.type=this.type;
+    ele.classList.add(this.type);
+    return ele;
+};
